99f28346e6993f94236cdbcc895d5f1fa763aff4
Jat2446 <109897160+Jat2446@users.noreply.github.com>
2023-11-22 22:33:42 +0530

    Add files via upload
    adding 404 page not found and using filtering technique

--- message.js
+++ message.js
@@ -3,23 +3,18 @@
 const express = require('express')
 // console.log(routes.someText)
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shop = require('./routes/shop')
+
 const bodyparser = require('body-parser')
-app.use(bodyparser.urlencoded())
+app.use(bodyparser.urlencoded({extended:false}))
 
-app.use('/add-product',(req,res,next) => {
-    console.log("I am another midleware")
-    res.send('<form action="/product" method="POST"><input type="text" name="message"><button type="submit">send</button></form>')
-    next();
-})
+app.use('/admin',adminRoutes);
+app.use(shop);
 
-app.post('/product',(req,res,next) => {
-    console.log(req.body)
-    res.redirect('/');
-})
-
-app.use('/',(req,res,next) => {
-    console.log("I am another midleware")
-    res.send("<h1> hello from express !</h1>")
+app.use((req,res,next) => {
+    res.status(404).send('<h1>Page not found</h1')
 })
 // const server = http.createServer(app)
 app.listen(5000);
